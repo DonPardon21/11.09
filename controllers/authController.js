1aab1e7f989f52c1b61482ccbe357205c535df2c
DonPardon21 <linetrox@onet.pl>
2024-12-10 15:10:18 +0100

    Logowanie działa z bazą

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,19 +1,30 @@
 const bcrypt = require("bcrypt");
-const { getDb } = require("../public/js/db");
+const mysql = require("mysql");
+
+// Konfiguracja połączenia z bazą danych
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "ksiegarnia",
+});
 
 // Rejestracja użytkownika
 async function registerUser(req, res) {
   const { login, password, email } = req.body;
 
   try {
-    const db = getDb();
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
 
-    const user = { login, password: hashedPassword, email };
-    await db.collection("users").insertOne(user);
-
-    res.status(201).send("Użytkownik zarejestrowany pomyślnie");
+    const query = "INSERT INTO users (Login, Password, Email) VALUES (?, ?, ?)";
+    db.query(query, [login, hashedPassword, email], (err) => {
+      if (err) {
+        console.error("Błąd rejestracji:", err.message);
+        return res.status(500).send("Wystąpił błąd");
+      }
+      res.status(201).send("Użytkownik zarejestrowany pomyślnie");
+    });
   } catch (err) {
     console.error("Błąd rejestracji:", err);
     res.status(500).send("Wystąpił błąd");
@@ -25,22 +36,31 @@ async function loginUser(req, res) {
   const { login, password } = req.body;
 
   try {
-    const db = getDb();
-    const user = await db.collection("users").findOne({ login });
+    const query = "SELECT * FROM users WHERE Login = ?";
+    db.query(query, [login], async (err, results) => {
+      if (err) {
+        console.error("Błąd logowania:", err.message);
+        return res.status(500).send("Wystąpił błąd");
+      }
 
-    if (!user || !(await bcrypt.compare(password, user.password))) {
-      return res.status(401).send("Nieprawidłowe dane logowania");
-    }
+      if (
+        results.length === 0 ||
+        !(await bcrypt.compare(password, results[0].Password))
+      ) {
+        return res.status(401).send("Nieprawidłowe dane logowania");
+      }
 
-    // Ustawienie ciasteczka i sesji
-    req.session.user = { login: user.login, lastLogin: new Date() };
-    res.cookie("lastLogin", new Date().toISOString(), { httpOnly: true });
+      // Ustawienie sesji i ciasteczek
+      req.session.user = { login: results[0].Login, lastLogin: new Date() };
+      res.cookie("lastLogin", new Date().toISOString(), { httpOnly: true });
+      res.cookie("message", "Zalogowano pomyślnie", {
+        httpOnly: false,
+        maxAge: 5000,
+      });
 
-    // Ustawienie ciasteczka z wiadomością o sukcesie
-    res.cookie("message", "Zalogowano pomyślnie", { httpOnly: false, maxAge: 5000 });
-
-    // Przekierowanie na stronę główną
-    res.redirect("/mainpage");
+      // Przekierowanie na stronę główną
+      res.redirect("/main");
+    });
   } catch (err) {
     console.error("Błąd logowania:", err);
     res.status(500).send("Wystąpił błąd");
